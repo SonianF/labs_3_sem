039c86994773f9e7dcefa007c79224d94a5970aa
SonianF <96430605+SonianF@users.noreply.github.com>
2023-11-27 08:17:58 +0400

    Add files via upload

---
+++ laba1/task 4/MyFirstProgram.java
@@ -0,0 +1,16 @@
+
+class MyFirstClass {
+public static void main (String [] s) {
+MySecondClass o = new MySecondClass();
+int i, j;
+for (i = 1; i <= 8; i++) {
+for(j = 1; j <= 8; j++) {
+o.setX (i);
+o.setY (j);
+System.out.print(o.summ());
+System.out.print(" ");
+}
+System.out.println();
+}
+}
+};
